103c5c7dc84c5fa9d7b2d938d7dd47d7759f1fc6
Sandeep <65333883+imsandeepk@users.noreply.github.com>
2024-12-29 21:44:37 +0530

    infer

--- scripts/data_prep/visualize.py
+++ scripts/data_prep/visualize.py
@@ -2,13 +2,13 @@ import os
 import numpy as np
 import open3d as o3d
 
-mesh_path = '/Users/sandeep/DOCS/Point Cloud Training/data/point_cloud_meshes/point_cloud_5.pcd'
-seg_path = '/Users/sandeep/DOCS/Point Cloud Training/data/point_cloud_labels/point_cloud_5.txt'
+mesh_path = "/Users/sandeep/DOCS/Point Cloud Training/predicted_labels.pcd"
+seg_path = "/Users/sandeep/DOCS/Point Cloud Training/predicted_labels.txt"
 
 mesh = o3d.io.read_point_cloud(mesh_path)
 seg = np.loadtxt(seg_path)
 
 colors = [[1,0,0] if x == 1 else [0,1,0] for x in seg]
 mesh.colors = o3d.utility.Vector3dVector(colors)
-
+print(len(mesh.points))
 o3d.visualization.draw_geometries([mesh])
